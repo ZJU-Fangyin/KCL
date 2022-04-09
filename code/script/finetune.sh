d43d99e49080c638d80886713fa13420196dc24f
Fangyin <61076726+ZJU-Fangyin@users.noreply.github.com>
2022-04-09 14:25:39 +0800

    Update finetune.sh

--- code/script/finetune.sh
+++ code/script/finetune.sh
@@ -4,8 +4,8 @@ CUDA_VISIBLE_DEVICES=1 python finetune.py \
     --batch_size 64 \
     --predictor_hidden_feats 32 \
     --patience 30 \
-    --encoder_path /home/fangyin/kcl/dump/0910-Pretrain/gnn-kmpnn-model/KMPNNGNN_0910_2302_78000th_epoch.pkl \
-    --readout_path /home/fangyin/kcl/dump/0910-Pretrain/gnn-kmpnn-model/Set2Set_0910_2302_78000th_epoch.pkl \
+    --encoder_path ./dump/0910-Pretrain/gnn-kmpnn-model/KMPNNGNN_0910_2302_78000th_epoch.pkl \
+    --readout_path ./dump/0910-Pretrain/gnn-kmpnn-model/Set2Set_0910_2302_78000th_epoch.pkl \
     --lr 0.01 \
     --predictor nonlinear \
     --eval nonfreeze \
