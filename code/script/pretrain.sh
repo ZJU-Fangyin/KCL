62ad655f5ebe30360de90b070612703e33bcad59
Fangyin <61076726+ZJU-Fangyin@users.noreply.github.com>
2022-04-14 22:21:28 +0800

    Update pretrain.sh

--- code/script/pretrain.sh
+++ code/script/pretrain.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0 python pretrain.py \
     --hidden_feats 64 \
     --num_gnn_layers 2 \
     --epoch_num 100 \
-    --data_name  cluster_0.85 \
+    --data_name  ../data/cluster_0.85 \
     --lmdb_env ../data/zinc15_250K_2D \
     --generator_process before_encoder \
     --dump_path ./dump \
