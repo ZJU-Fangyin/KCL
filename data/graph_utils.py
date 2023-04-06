e0eb798cad1b227499b6b67922691cd33660eb99
Fangyin <61076726+ZJU-Fangyin@users.noreply.github.com>
2023-04-06 14:58:03 +0800

    Update graph_utils.py

--- data/graph_utils.py
+++ data/graph_utils.py
@@ -15,6 +15,9 @@ import pdb
 from load_triples import Triples
 from torch.nn import Embedding
 from pandarallel import pandarallel
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
+from rdkit.ML.Cluster import Butina
 
 
 def bondtype_features(bond):
@@ -25,7 +28,7 @@ def bondtype_features(bond):
     fbond = bond2emb[str(bond.GetBondType())]
     return fbond
 
-def smiles_2_kgdgl(smiles):
+def smiles_2_kgdgl(smiles): # augmented graph
     data = Triples()
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
@@ -42,9 +45,9 @@ def smiles_2_kgdgl(smiles):
     atoms_feature = [0 for _ in range(len(connected_atom_list))]
 
     # get all node ids and relations
-    begin_atoms = []
-    end_entities = []
-    rel_features = []
+    begin_attributes = []    # attributes
+    end_atoms = []   # atoms
+    rel_features = []   # relations between attributes and atoms
     for atom in mol.GetAtoms():
         node_index = atom.GetIdx()
         symbol = atom.GetSymbol()
@@ -52,24 +55,25 @@ def smiles_2_kgdgl(smiles):
         if node_index not in connected_atom_list:
             continue
 
-        atoms_feature[connected_atom_map[node_index]] = atomicnum
+        atoms_feature[connected_atom_map[node_index]] = atomicnum # atom nodes indexed by atomicnum
 
         if symbol in data.entities:
-            tid = [t for (r,t) in data.h2rt[data.entity2id[symbol]]]
-            rid = [r for (r,t) in data.h2rt[data.entity2id[symbol]]]
+            attribute_id = [h for (r,h) in data.t2rh[data.entity2id[symbol]]] 
+            rid = [r for (r,h) in data.t2rh[data.entity2id[symbol]]] # relation ids 
 
-            begin_atoms.extend([node_index]*len(tid))   # add head entities
-            end_entities.extend(tid)    # add tail eneities
-            rel_features.extend(i+4 for i in rid)
+            begin_attributes.extend(attribute_id)   # add attribute ids
+            end_atoms.extend([node_index]*len(attribute_id))    # add atom ids
+            rel_features.extend(i+4 for i in rid)  # first 4 ids are prepared for bonds, relation ids begin after bond ids
 
-    # get list of tail entity ids and features
-    if end_entities:
-        entity_id = sorted(list(set(end_entities)))
-        node_id = [i+len(connected_atom_list) for i in range(len(entity_id))]
-        entid2nodeid = dict(zip(entity_id, node_id)) # dict: t_id in triples --> node_id in dglgraph
-        nodeids = [entid2nodeid[i] for i in end_entities]   # list of tail entity id
 
-        nodes_feature = [i+104 for i in entity_id]
+    # get list of attribute ids and features
+    if begin_attributes:
+        attribute_id = sorted(list(set(begin_attributes)))
+        node_id = [i+len(connected_atom_list) for i in range(len(attribute_id))]
+        attrid2nodeid = dict(zip(attribute_id, node_id)) # dict: attribute_id in triples --> node_id in dglgraph
+        nodeids = [attrid2nodeid[i] for i in begin_attributes] # list of attribute ids
+
+        nodes_feature = [i+118 for i in attribute_id] # first 118 ids are prepared for atoms, attribute ids begin after atom ids
 
     # get list of atom ids and bond features
     begin_indexes = []
@@ -89,10 +93,10 @@ def smiles_2_kgdgl(smiles):
         bonds_feature.append(bond_feature)
     edge_type.extend([0]*len(bonds_feature))
 
-    # add ids and features of tail entities and relations
-    if begin_atoms:
-        begin_indexes.extend(nodeids) # change head and tail entities
-        end_indexes.extend(begin_atoms)
+    # add ids and features of attributes and relations
+    if end_atoms:
+        begin_indexes.extend(nodeids) 
+        end_indexes.extend(end_atoms)
         atoms_feature.extend(nodes_feature)
         bonds_feature.extend(rel_features)
         edge_type.extend([1]*len(rel_features))
@@ -105,7 +109,7 @@ def smiles_2_kgdgl(smiles):
     graph.edata['etype'] = torch.tensor(edge_type, dtype=torch.long) # 0 for bonds & 1 for rels
     return graph
 
-
+    
 def smiles_2_dgl(smiles):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
@@ -158,9 +162,9 @@ if __name__ == '__main__':
 
     pandarallel.initialize()
     data = pd.read_csv(data_path)
-    # 创建数据库文件
+    # construct db
     env = lmdb.open(f'./zinc15_250K_2D', map_size=int(1e12), max_dbs=2)
-    # 创建对应的数据库
+    # construct db for the original molecular graph & the augmented molecular graph
     graphs_db = env.open_db('graph'.encode())
     kgraphs_db = env.open_db('kgraph'.encode())
 
@@ -169,20 +173,10 @@ if __name__ == '__main__':
 
     kgraphs = data['smiles'].parallel_apply(smiles_2_kgdgl).to_list()
     kgraphs = list(filter(None, kgraphs))
-    # 把数据写入到LMDB中
+    # write into db
     with env.begin(write=True) as txn:
         for idx, graph in tqdm(enumerate(graphs)):
             txn.put(str(idx).encode(), pickle.dumps(graph), db=graphs_db)
         for idx, kgraph in tqdm(enumerate(kgraphs)):
             txn.put(str(idx).encode(), pickle.dumps(kgraph), db=kgraphs_db)
     env.close()
-
-    import random
-    import pickle
-    i = list(range(len(graphs)))
-    random.shuffle(i)
-
-    with open('zinc15_250K_2D.pkl', 'wb') as f:
-        pickle.dump(i, f)
-
-
